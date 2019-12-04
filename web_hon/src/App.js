5e113332cd6c106c97d94057a2af5767107c1358
maman <mamanabdurrahman11@gmail.com>
2019-12-04 14:16:50 +0700

    display text
    Signed-off-by: maman <mamanabdurrahman11@gmail.com>

--- web_hon/src/App.js
+++ web_hon/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     sentence: ''
+  //   }
+  // }
+  state = {
+    sentence: ''
+  }
+
+  getResponse = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  }
+
+  componentDidMount() {
+    this.getResponse()
+      .then(res => {
+        const someData = res;
+        this.setState({ sentence: someData.data });
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          {this.state.sentence}
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
