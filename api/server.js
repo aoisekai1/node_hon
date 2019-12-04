690440f0899c60244785cd7babff8452152ae4a6
maman <mamanabdurrahman11@gmail.com>
2019-12-04 14:24:23 +0700

    update data api
    Signed-off-by: maman <mamanabdurrahman11@gmail.com>

--- api/server.js
+++ api/server.js
@@ -7,8 +7,14 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = {
+    'nama' : 'Rusli Amrin',
+    'jekel' : 'Laki-Laki',
+    'alamat' : 'Jln. Mencari gratisan'
+}
+
 app.get('/api/hello', (req, res) => {
-    res.send({data: "Testing first api with node js and express"});
+    res.send({data: data});
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
