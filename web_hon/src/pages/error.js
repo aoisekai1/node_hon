84d1d51ac809095a33f6995075f24b9d3bf13ca9
maman <mamanabdurrahman11@gmail.com>
2019-12-05 23:56:00 +0700

    error page
    Signed-off-by: maman <mamanabdurrahman11@gmail.com>

---
+++ web_hon/src/pages/error.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class NoMatch extends Component{
+    render(){
+        return(
+            <center>
+                <h1>404</h1>
+                <label>Page Not Found</label>
+            </center>
+        )
+    }
+}
+
+export default NoMatch;
