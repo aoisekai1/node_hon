1621f726fa8431cb924441823a98fa922a4b19a4
maman <mamanabdurrahman11@gmail.com>
2019-12-05 23:57:47 +0700

    main api
    Signed-off-by: maman <mamanabdurrahman11@gmail.com>

--- api/server.js
+++ api/server.js
@@ -1,20 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+const posts = require('../api/posts/posts');
 
-const data = {
-    'nama' : 'Rusli Amrin',
-    'jekel' : 'Laki-Laki',
-    'alamat' : 'Jln. Mencari gratisan'
-}
-
-app.get('/api/hello', (req, res) => {
-    res.send({data: data});
-})
+app.use('/api', posts);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
